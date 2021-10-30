6dbd1af3708c794a4577f7101f30c78c0bc893e2
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-10-30 08:41:18 -0600

    Added playlists config file

---
+++ config/playlists.py
@@ -0,0 +1,7 @@
+def spotify_playlist_1():
+    playlists = {'Rap_Caviar': 'spotify:playlist:37i9dQZF1DX0XUsuxWHRQd'}
+    return playlists
+
+def spotify_playlist_2():
+    playlists = {'Operator': 'spotify:playlist:37i9dQZF1DWVY4eLfA3XFQ'}
+    return playlists
