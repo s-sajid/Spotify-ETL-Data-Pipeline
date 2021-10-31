e77568ed6c124b56014bdcb3decccf0f75d66a9c
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-10-31 14:32:40 -0600

    Output Dictionary Initialize

--- spotify_playlists.py
+++ spotify_playlists.py
@@ -4,3 +4,15 @@ import boto3
 from datetime import datetime
 
 from config.playlists import spotify_playlist_1
+## Insert Tools
+
+spotipy_object = spotipy.Spotify(client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials())
+
+output_dictionary = {
+    "Year_Released" : [],
+    "Album_Length" : [],
+    "Album_Name" : [],
+    "Artist" : []
+}
+
+PLAYLIST = "Rap_Caviar"
