b943ab104245b020021d415bf031a9b4f0bdcbea
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-11-01 10:42:06 -0600

    Local Data Function

--- spotify_playlists.py
+++ spotify_playlists.py
@@ -6,13 +6,21 @@ from datetime import datetime
 from config.playlists import spotify_playlist_1
 ## Insert Tools
 
-spotipy_object = spotipy.Spotify(client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials())
+# spotipy_object = spotipy.Spotify(client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials())
 
-output_dictionary = {
+
+
+PLAYLIST = "Rap_Caviar"
+
+def gather_data_local():
+    output_dictionary = {
     "Year_Released" : [],
     "Album_Length" : [],
     "Album_Name" : [],
     "Artist" : []
 }
-
-PLAYLIST = "Rap_Caviar"
+    with open("Rap_Caviar_Albums.csv", "w") as file:
+        header = list( output_dictionary.keys())
+        writer = csv.DictWriter(file, fieldnames = header)
+        writer.writeheader()
+        albums_obtained = []
