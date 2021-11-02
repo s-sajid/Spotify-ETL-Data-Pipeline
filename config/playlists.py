b1c52f6f91dc52ab03d414b38cf06c02b1a67b39
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-11-02 10:44:16 -0600

    Added Personal Playlists

--- config/playlists.py
+++ config/playlists.py
@@ -1,7 +1,15 @@
 def spotify_playlist_1():
-    playlists = {'Rap_Caviar': 'spotify:playlist:37i9dQZF1DX0XUsuxWHRQd'}
+    playlists = {"Rap_Caviar": "spotify:playlist:37i9dQZF1DX0XUsuxWHRQd"}
     return playlists
 
 def spotify_playlist_2():
-    playlists = {'Operator': 'spotify:playlist:37i9dQZF1DWVY4eLfA3XFQ'}
+    playlists = {"Operator": "spotify:playlist:37i9dQZF1DWVY4eLfA3XFQ"}
+    return playlists
+
+def personal_playlist_1():
+    playlists = {"Hip Hop Playlist 1": "spotify:playlist:71sU6agJ13FCerhUW6tlll"}
+    return playlists
+
+def personal_playlist_2():
+    playlists = {"EDM Playlist 1": "spotify:playlist:16bWFZT4i3MJzIsdoq2Su8"}
     return playlists
