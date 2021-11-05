0101ccf1a2be32fc92ba5583d851e6b7967ac0ec
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-11-05 11:56:22 -0600

    Added Local Data Gather Function

--- spotify_playlists.py
+++ spotify_playlists.py
@@ -16,16 +16,46 @@ from tools.playlists import get_artists
 PLAYLIST = "Rap_Caviar"
 
 def gather_data_local():
-    output_dictionary = {
+    output_dict = {
     "Year_Released" : [],
     "Album_Length" : [],
     "Album_Name" : [],
     "Artist" : []
 }
     with open("Rap_Caviar_Albums.csv", "w") as file:
-        header = list( output_dictionary.keys())
+        header = list( output_dict.keys())
         writer = csv.DictWriter(file, fieldnames = header)
         writer.writeheader()
         albums_obtained = []
 
         artists = get_artists(spotify_playlist_1()[PLAYLIST])
+
+        for artist in list(artists.keys()):
+            print(artist)
+            artists_albums = spotipy_object.artist_albums(artist, album_type="album", limit = 50)
+
+            for album in artists_albums["items"]:
+
+                if "US" in album["available_markets"]:
+                    key = album["name"] + album["artists"][0]["name"] + album["release_date"][:4]
+
+                    if key not in albums_obtained:
+                        albums_obtained.append(key)
+                        album_data = spotipy_object.album(album["uri"])
+
+                        album_length_ms = 0
+
+                        for song in album_data["tracks"]["items"]:
+                            album_length_ms = song["duration_ms"] + album_length_ms
+                            
+                        writer.writerow({"Year Released": album_data["release_date"][:4],
+                            "Album Length": album_length_ms,
+                            "Album Name": album_data["name"],
+                            "Artist": album_data["artists"][0]["name"]})
+                    
+                        output_dict["Year Release"].append(album_data["release_date"][:4])
+                        output_dict["Album Length"].append(album_length_ms)
+                        output_dict["Album_Name"].append(album_data["name"])
+                        output_dict["Artist"].append(album_data["artists"][0]["name"])
+    
+    return output_dict
