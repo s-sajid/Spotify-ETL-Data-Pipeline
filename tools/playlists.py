ffd942c0d11ff65c9dab8da2ca80e4f49ddb2591
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-11-06 14:57:08 -0600

    Added Authentication Ids

--- tools/playlists.py
+++ tools/playlists.py
@@ -1,14 +1,16 @@
 import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+load_dotenv()
 
-# spotify = spotipy.Spotify(client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials())
+spotify = spotipy.Spotify(client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(os.getenv("Client_Id"), os.getenv("Client_Secret")))
 
 def get_artists(playlist_uri):
     artists = {}
-    playlist_tracks = spotipy.playlist_tracks(playlist_id = playlist_uri)
-
-    for song in playlist_tracks["items"]:
-        if song["track"]:
-            print(song["track"]["artists"][0]["name"])
-            artists[song["track"]["artists"][0]["uri"]] = song["track"]["artists"][0]["name"]
-    
+    playlist_tracks = spotify.playlist_tracks(playlist_id=playlist_uri)
+    for song in playlist_tracks['items']:
+        if song['track']:
+            print(song['track']['artists'][0]['name'])
+            artists[song['track']['artists'][0]['uri']] = song['track']['artists'][0]['name']
     return artists
