07af3ab3c63bba1130247b7c41370f30db53a142
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-11-20 21:11:45 -0700

    Added Lambda Trust Policy

--- terraform/iam_lambda.tf
+++ terraform/iam_lambda.tf
@@ -3,6 +3,21 @@ resource "aws_iam_role" "lambda_execution_role"{
     assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy.json
 }
 
+data "aws_iam_policy_document" "lambda_trust_policy" {
+    statement {
+        effect = "Allow"
+
+        principals {
+            type = "Service"
+            identifiers = [
+                "lambda.amazonaws.com",
+                "ec2.amazonaws.com"
+            ]
+        }
+        actions = ["sts:AssumeRole"]
+    }
+}
+
 resource "aws_iam_policy" "lambda_execution_policy" {
   name = "lambda_execution_policy"
   policy = data.aws_iam_policy_document.lambda_execution_policy_document.json
