3ff1c00f2eb5d9d86e2028d32d371d121430a921
Shadman Sajid <88870507+s-sajid@users.noreply.github.com>
2021-12-02 12:38:34 -0700

    Added Files

--- terraform/aws_architecture_plan.sh
+++ terraform/aws_architecture_plan.sh
@@ -1,4 +1,14 @@
-terraform init
+# terraform init
 
 cp -r /Users/sdsaj/Anaconda3/Lib/site-packages/spotipy ../lambda_payloads/playlist_payload/
 cp -r /Users/sdsaj/Anaconda3/Lib/site-packages/requests ../lambda_payloads/playlist_payload/
+
+cp /Users/sdsaj/Desktop/Python/Spotify_Data_Pipeline/spotify_playlists.py ../lambda_payloads/playlist_payload/
+cp /Users/sdsaj/Desktop/Python/Spotify_Data_Pipeline/config/playlists.py ../lambda_payloads/playlist_payload/config
+cp /Users/sdsaj/Desktop/Python/Spotify_Data_Pipeline/tools/playlists.py ../lambda_payloads/playlist_payload/tools
+
+# terraform validate
+
+# terraform plan
+
+# terraform apply
